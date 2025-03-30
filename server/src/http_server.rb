a5eaa3829770b7fd952c599b7ceff3a315247728
Erick Contreras <er.contreras@hotmail.com>
2025-03-30 14:22:04 -0600

    Fix linters

--- server/src/http_server.rb
+++ server/src/http_server.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 require 'socket'
 require 'uri'
-require_relative './modules/constants.rb'
-require_relative './modules/data_sender.rb'
-require_relative './modules/data_handler.rb'
+require_relative './modules/constants'
+require_relative './modules/data_sender'
+require_relative './modules/data_handler'
 
+# HTTP Server Class
 class HttpServer
   include DataHandler
   include DataSender
-  
+
   def initialize
     @server = TCPServer.new('0.0.0.0', 3000)
   end
