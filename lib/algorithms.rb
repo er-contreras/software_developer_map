9f59d33dbca31363c769e888d744714f2262a054
Erick Contreras <er.contreras@hotmail.com>
2025-04-01 17:39:00 -0600

    Fix linterns

--- lib/algorithms.rb
+++ lib/algorithms.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Algos Module for algorithms
 module Algos
   def insertion_sort(arr)
     key = arr[1]
@@ -5,11 +8,10 @@ module Algos
     i = 0
     while i < arr.length
       j = i + 1
-      while i > -1 && arr[i] < key
-        arr[j + 1] = key
-      end
 
-      arr[i + 1] = arr[i] 
+      arr[j + 1] = key while i > -1 && arr[i] < key
+
+      arr[i + 1] = arr[i]
       i += 1
     end
 
@@ -17,10 +19,9 @@ module Algos
   end
 
   def selection_sort; end
-
   def merge_sort; end
-
+  def heap_sort; end
   def bubble_sort; end
-
+  def quick_sort; end
   def linked_list; end
 end
