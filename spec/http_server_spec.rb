c873aa5020477cbaa360041dca04433a5ca38e58
Erick Contreras <er.contreras@hotmail.com>
2025-04-04 22:20:21 -0600

    Add test for server setup

--- spec/http_server_spec.rb
+++ spec/http_server_spec.rb
@@ -15,4 +15,26 @@ RSpec.describe HttpServer do
       expect(server.instance_variable_get(:@server)).to be_nil
     end
   end
+
+  describe '#setup_server' do
+    let(:mock_tcp_server) { instance_double(TCPServer, close: nil) }
+
+    before do
+      allow(TCPServer).to receive(:new).with(host, port).and_return(mock_tcp_server)
+    end
+
+    it 'creates a TCPServer with the correct host and port' do
+      expect(TCPServer).to receive(:new).with(host, port).and_return(mock_tcp_server)
+      server.setup_server
+    end
+
+    it 'assigns the created server to @server' do
+      server.setup_server
+      expect(server.instance_variable_get(:@server)).to eq(mock_tcp_server)
+    end
+
+    it 'prints a listening message' do
+      expect { server.setup_server }.to output(/Server listening on #{host}:#{port}/).to_stdout
+    end
+  end
 end
