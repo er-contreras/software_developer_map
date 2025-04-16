53a50b952386c90ff61a1ad4fba1a237908110fd
Erick Contreras <er.contreras@hotmail.com>
2025-04-16 00:59:29 -0600

    Fix readability

--- spec/http_server_spec.rb
+++ spec/http_server_spec.rb
@@ -10,7 +10,12 @@ RSpec.describe HttpServer do
 
   subject(:server) { described_class.new(host, port) }
 
-  let(:mock_tcp_server) { instance_double(TCPServer, close: nil) }
+  let(:mock_tcp_server) do
+    instance_double(
+      TCPServer,
+      close: nil
+    )
+  end
 
   describe '#initialize' do
     it 'initializes with host and port' do
@@ -20,44 +25,75 @@ RSpec.describe HttpServer do
 
   describe '#setup_server' do
     before do
-      allow(TCPServer).to receive(:new).with(host, port).and_return(mock_tcp_server)
+      allow(TCPServer)
+        .to receive(:new)
+        .with(host, port)
+        .and_return(mock_tcp_server)
     end
 
     it 'creates a TCPServer with the correct host and port' do
-      expect(TCPServer).to receive(:new).with(host, port).and_return(mock_tcp_server)
+      expect(TCPServer)
+        .to receive(:new)
+        .with(host, port)
+        .and_return(mock_tcp_server)
+
       server.setup_server
     end
 
     it 'assigns the created server to @server' do
       server.setup_server
+
       expect(server.instance_variable_get(:@server)).to eq(mock_tcp_server)
     end
 
     it 'prints a listening message' do
-      expect { server.setup_server }.to output(/Server listening on #{host}:#{port}/).to_stdout
+      expect do
+        server.setup_server
+      end
+        .to output(/Server listening on #{host}:#{port}/)
+        .to_stdout
     end
   end
 
   describe '#setup_server when port is in use' do
     before do
-      allow(TCPServer).to receive(:new).with(host, port).and_raise(Errno::EADDRINUSE)
+      allow(TCPServer)
+        .to receive(:new)
+        .with(host, port)
+        .and_raise(Errno::EADDRINUSE)
+
       allow(server).to receive(:exit).with(1)
     end
 
     it 'failes because of port is in use' do
-      expect { server.setup_server }.to output(/Error: Port #{port} already in use./).to_stdout
+      expect do
+        server.setup_server
+      end
+        .to output(/Error: Port #{port} already in use./)
+        .to_stdout
+
       expect(server).to have_received(:exit).with(1)
     end
   end
 
   describe '#start' do
     let(:mock_client) do
-      instance_double(TCPSocket, peeraddr: ['AF_INET', 12_345, '127.0.0.1', '127.0.0.1'], close: nil)
+      instance_double(
+        TCPSocket,
+        peeraddr: ['AF_INET', 12_345, '127.0.0.1', '127.0.0.1'],
+        close: nil
+      )
     end
 
     before do
-      allow(TCPServer).to receive(:new).and_return(mock_tcp_server)
-      allow(mock_tcp_server).to receive(:accept).and_return(mock_client).and_raise(Interrupt)
+      allow(TCPServer)
+        .to receive(:new)
+        .and_return(mock_tcp_server)
+
+      allow(mock_tcp_server)
+        .to receive(:accept)
+        .and_return(mock_client)
+        .and_raise(Interrupt)
 
       calls = 0
       allow(mock_tcp_server).to receive(:accept) do
